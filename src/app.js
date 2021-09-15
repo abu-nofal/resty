27842353beb30ac6a0a3650c65d3d11f0ff6ef4e
abu-nofal <anofal719@gmail.com>
2021-09-15 15:08:22 +0300

    lab29 done

--- src/app.js
+++ src/app.js
@@ -1,61 +1,90 @@
-import React from 'react';
-import axios from 'axios';
-import './app.scss';
-import { useState ,useEffect } from 'react';
+import React from "react";
+import axios from "axios";
+import "./app.scss";
+import { useState, useEffect, useReducer } from "react";
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form';
-import Results from './components/results';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Form from "./components/form";
+import Results from "./components/results";
+import { HISTORY } from "./components/history";
 
-function  App (props) {
+const initialState = {
+  history: [],
+};
+function historyReducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case "ADD_HISTORY":
+      const history = [...state.history, payload];
+      return { history };
+    default:
+      return state;
+  }
+}
+function addAction(obj) {
+  return {
+    type: "ADD_HISTORY",
+    payload: { obj },
+  };
+}
 
-  const [requestParams,setRequestParams]=useState({})
-  const [result,setResult]=useState([])
-  const [render,setRender]=useState('')
+function App(props) {
+  const [requestParams, setRequestParams] = useState({});
+  const [result, setResult] = useState(null);
+  const [render, setRender] = useState("");
+  const [state, dispatch] = useReducer(historyReducer, initialState);
 
-   const callApi = (requestParams) => {
+  const callApi = (requestParams) => {
     // mock output
     console.log(requestParams);
-    let reqBody=requestParams.reqBody
-    let method=requestParams.method
-    let url=requestParams.url
-    if(method=='post'||method=='put'){
-      axios[method](url,reqBody).then(results=>{
-        setResult([...result,results.data])
-        setRequestParams({...requestParams,requestParams})
-        //  requestParams:requestParams
-      })
-    }else{
-      axios[method](url).then(results=>{
-        setResult([...result,results.data])
-        setRequestParams({...requestParams,requestParams})
-        console.log(result);
-      })
+    let reqBody = requestParams.reqBody;
+    let method = requestParams.method;
+    let url = requestParams.url;
+    if (method == "post" || method == "put") {
+      axios[method](url, reqBody).then((results) => {
+        setResult(results.data);
+        setRequestParams({ ...requestParams, requestParams });
+      });
+      dispatch(
+        addAction({
+          method: method,
+          url: url,
+          reqBody: reqBody,
+        })
+      );
+    } else {
+      axios[method](url).then((results) => {
+        setResult(results.data);
+        setRequestParams({ ...requestParams, requestParams });
+      });
+      dispatch(
+        addAction({
+          method: method,
+          url: url,
+        })
+      );
     }
-     
-  }
+  };
 
   useEffect(() => {
+    setRender(`method : ${requestParams.method}, URL : ${requestParams.url}`);
+  });
 
-      setRender(`method : ${requestParams.method}, URL : ${requestParams.url}`)
-    })
-
- 
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {requestParams.method}</div>
-        <div>URL: {requestParams.url}</div>
-        <div>i run from useEffect : {render}</div>
-        <Form handleApiCall={callApi} />
-        <Results data={result} />
-        <Footer />
-      </React.Fragment>
-    );
- 
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Request Method: {requestParams.method}</div>
+      <div>URL: {requestParams.url}</div>
+      <div>i run from useEffect : {render}</div>
+      <HISTORY history={state.history} handleApiCall={callApi} />
+      <Form handleApiCall={callApi} />
+      <Results data={result} />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
